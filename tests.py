519a472fcf066af8631ffac1238c49bbfc526117
DmitriyVypovskoi <davypovskoy@miem.hse.ru>
2021-12-18 19:19:22 +0300

    project v4

--- tests.py
+++ tests.py
@@ -4,9 +4,8 @@ from main import weather1
 from main import help1
 from unittest import IsolatedAsyncioTestCase
 from unittest.mock import MagicMock
-
-
-
+import json
+from mock import Mock, patch, MagicMock
 
 async def async_magic():
     pass
@@ -24,18 +23,26 @@ class MyTestCase(unittest.IsolatedAsyncioTestCase):
                                           "если она была заглавной и наоборот")
 
 
-class MyTestCase2(unittest.IsolatedAsyncioTestCase):
     async def test_something2(self):
         message = MagicMock()
         await start1(message=message)
         message.reply.assert_called_with("Привет, введите город в котором хотите узнать погоду")
 
 
-class MyTestCase3(unittest.IsolatedAsyncioTestCase):
     async def test_something3(self):
         message = MagicMock()
         await weather1(message=message)
         message.reply.assert_called_with("Проверьте правильность введённых данных")
+
+    @patch('requests.get')
+    async def test_weather(self, get):
+        txt = '{"coord":{"lon":38.9769,"lat":45.0328},"weather":[{"id":803,"main":"Clouds","description":"broken clouds","icon":"04n"}],"base":"stations","main":{"temp":2.2,"feels_like":2.2,"temp_min":1.68,"temp_max":2.7,"pressure":1015,"humidity":92},"visibility":10000,"wind":{"speed":1,"deg":280},"clouds":{"all":75},"dt":1639843462,"sys":{"type":2,"id":2012251,"country":"RU","sunrise":1639803462,"sunset":1639835023},"timezone":10800,"id":542420,"name":"Krasnodar","cod":200}'
+        response = MagicMock()
+        response.json.return_value = json.loads(txt)
+        get.return_value = response
+        message = MagicMock()
+        await weather1(message=message)
+        message.reply.assert_called_with("Погода в городе: Krasnodar\nCтрана: RU\nОблачно☁\nТемпература: 2С°\nОщущается как: 2С°\nВлажность: 92%\nДавление: 1015мм.рт.ст\nСкорость ветра: 1м/с\nВосход солнца: 2021-12-18 07:57:42\nЗакат солнца: 2021-12-18 16:43:43\nПродолжительность светового дня: 8:46:01")
 
 
 

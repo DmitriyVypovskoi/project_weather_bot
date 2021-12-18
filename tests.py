1d6ac96768d1941749cb01962ba317fd11175a85
DmitriyVypovskoi <davypovskoy@miem.hse.ru>
2021-12-18 18:59:21 +0300

    project v3

--- tests.py
+++ tests.py
@@ -1,49 +1,41 @@
 import unittest
-import tg_bot
-#from bot import help
-#from bot import start
-#from bot import weather
-from unittest import mock
-from config import token
+from main import start1
+from main import weather1
+from main import help1
+from unittest import IsolatedAsyncioTestCase
 from unittest.mock import MagicMock
 
-class Test2(unittest.TestCase):
-    @mock.patch('tg_bot.bot')
-    def test_main_encode(self, tg_bot):
-        chat = MagicMock(id=123)
-        message = MagicMock(text="/help", chat=chat)
-        tg_bot.mess(message)
-        tg_bot.send_message.assert_called_with(123,
-                                                f"***После начала работы бота введите название города, в котором вы хотите узнать состояние погоды на данный момент.\nЯзык при этом не важен, можно указать как на русском так и на английском.\nЕсли найден не тот город попробуйте изменить первую букву на строчную если она была заглавной и наоборот***")
-
-"""
-class Test1(unittest.TestCase):
-
-    def test3(self):
-        self.assertNotEqual((weather("краснодар", token)), "Проверьте правильность введённых данных")
-
-    def test4(self):
-        self.assertNotEqual((weather("краснодар", token)), "Проверьте правильность введённых данных")
-
-    def test5(self):
-        self.assertIn(weather("ошибка", token), "Проверьте правильность введённых данных")
-
-    def test6(self):
-        self.assertNotEqual(weather("MOSCOW", token), "Проверьте правильность введённых данных")
-
-    def test7(self):
-        self.assertIn(weather("НЬЮЙОРК", token), "Проверьте правильность введённых данных")
 
 
-class Test2(unittest.TestCase):
 
-    def test_other2(self):
-        self.assertEqual(help(), "***После начала работы бота введите название города,"
-                         " в котором вы хотите узнать состояние погоды на данный момент.\n"
-                         "Язык при этом не важен, можно указать как на русском так и на английском.\n"
-                         "Если найден не тот город попробуйте изменить первую букву на строчную "
-                         "если она была заглавной и наоборот***")
+async def async_magic():
+    pass
 
-    def test_other3(self):
-        self.assertNotEqual(start("Voronezh"), "Проверьте правильность введённых данных")
-"""
+MagicMock.__await__ = lambda x: async_magic().__await__()
+
+
+class MyTestCase(unittest.IsolatedAsyncioTestCase):
+    async def test_something(self):
+        message = MagicMock()
+        await help1(message=message)
+        message.answer.assert_called_with("После начала работы бота введите название города, в котором вы хотите узнать состояние погоды на данный момент.\n"
+                                          "Язык при этом не важен, можно указать как на русском так и на английском.\n"
+                                          "Если найден не тот город попробуйте изменить первую букву на строчную "
+                                          "если она была заглавной и наоборот")
+
+
+class MyTestCase2(unittest.IsolatedAsyncioTestCase):
+    async def test_something2(self):
+        message = MagicMock()
+        await start1(message=message)
+        message.reply.assert_called_with("Привет, введите город в котором хотите узнать погоду")
+
+
+class MyTestCase3(unittest.IsolatedAsyncioTestCase):
+    async def test_something3(self):
+        message = MagicMock()
+        await weather1(message=message)
+        message.reply.assert_called_with("Проверьте правильность введённых данных")
+
+
+
